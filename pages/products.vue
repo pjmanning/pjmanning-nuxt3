8ed3a977ebe4abcfdca38acf2126e47030ee60cd
PJ Manning <phil.manning11@gmail.com>
2022-07-23 14:03:30 +0800

    headers for courses, products, templates

--- pages/products.vue
+++ pages/products.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>Products</div>
+  <div class="mb-8">
+    <div class="relative pt-4 sm:px-6 md:pt-12 lg:px-8">
+      <div class="absolute inset-0">
+        <div class="h-1/3 bg-white sm:h-2/3"></div>
+      </div>
+      <div class="relative mx-auto max-w-7xl">
+        <div>
+          <h2 class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-3xl sm:leading-10">Products</h2>
+          <p class="mt-3 max-w-2xl text-lg leading-7 text-gray-500 sm:mt-4">Resources to jump start your business...</p>
+          <div class="mt-12 h-px w-16 rounded bg-gray-300"></div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
