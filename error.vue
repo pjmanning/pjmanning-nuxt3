daf951e9a67cae59210d688419403b902498ea86
PJ Manning <phil.manning11@gmail.com>
2023-01-26 17:45:58 -0500

    test with new output

--- error.vue
+++ error.vue
@@ -8,7 +8,7 @@ defineProps({
   <div id="error" class="flex h-screen flex-col justify-center bg-gray-50 sm:px-6 lg:px-8">
     <div class="h-96 sm:mx-auto sm:w-full sm:max-w-md">
       <nuxt-img format="webp" class="mx-auto h-20 w-auto rounded-full text-white" src="/images/home/working.jpg" alt="pj manning" />
-      <h1 v-if="error.statusCode === 404" class="mt-6 text-center text-3xl font-extrabold leading-9 text-gray-900">Wipeout! 🌊 🏄🏽‍♂️</h1>
+      <h1 v-if="error.statusCode === 404" class="mt-6 text-center text-3xl font-extrabold leading-9 text-gray-900">Wipeout!! 🌊 🏄🏽‍♂️</h1>
       <h1 v-else class="mt-6 text-center text-3xl font-extrabold leading-9 text-gray-900">An error occurred</h1>
       <NuxtLink to="/" class="focus:shadow-outline-blue mt-12 flex w-full items-center justify-center rounded-md border border-transparent bg-blue-600 px-6 py-3 font-medium leading-6 text-white transition duration-150 ease-in-out hover:bg-blue-500 focus:border-blue-700 focus:outline-none active:bg-blue-700"
         >Back to Home Page
